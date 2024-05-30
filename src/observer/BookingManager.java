263df89a7d8d85361bcb2f84065a52df34f9ac8a
Deepak Ghalley <12210005.gcit@rub.edu.bt>
2024-05-30 12:44:03 +0000

    Upload New File

---
+++ src/observer/BookingManager.java
@@ -0,0 +1,33 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import BookingSystem.*;
+
+public class BookingManager {
+    private static BookingManager instance;
+    private List<Observer> observers = new ArrayList<>();
+
+    private BookingManager() {}
+
+    public static BookingManager getInstance() {
+        if (instance == null) {
+            instance = new BookingManager();
+        }
+        return instance;
+    }
+
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void unregisterObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+}
